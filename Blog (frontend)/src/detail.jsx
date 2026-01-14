936b89a90da15672af20fe95979a140fe7121846
AniketDandhare22 <aniketdandhare22@gmail.com>
2026-01-15 00:15:44 +0530

    uifix

--- Blog (frontend)/src/detail.jsx
+++ Blog (frontend)/src/detail.jsx
@@ -80,10 +80,10 @@ function Detail() {
     
     if (!post) {
     return (
-      <div className="w-full min-h-screen  bg-secondaryD light:bg-secondary ">
+      <div className="w-full min-h-screen flex bg-secondaryD light:bg-secondary ">
         <Navbar/>
         <div className="items-center justify-center flex-col w-full h-full">
-          <img className="w-10 animate-pulse" src="./assets/Screenshot 2026-01-14 234006.png" alt="" />
+          <img className="w-50 animate-pulse" src="./assets/Screenshot 2026-01-14 234006.png" alt="" />
           <span className="loading-dots px-2 text-lg font-semibold text-orange-400 light:text-purple-800">Loading </span>
         </div>
       </div>
