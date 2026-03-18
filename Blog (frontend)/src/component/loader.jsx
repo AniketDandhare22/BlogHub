5424a8f110a37d920930414ef5d993ff2fb81504
AniketDandhare22 <aniketdandhare22@gmail.com>
2026-03-18 23:07:40 +0530

    ui fixes in loading 2

--- Blog (frontend)/src/component/loader.jsx
+++ Blog (frontend)/src/component/loader.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import styled from 'styled-components';
 import { useTheme } from "../context/ThemeContext";
 
-const { dark } = useTheme();
 const Loader = () => {
+  const { dark } = useTheme(); // ✅ inside component
+
   return (
-    <StyledWrapper>
+    <StyledWrapper dark={dark}>
       <div className="cube">
         <div className="cube_item cube_x" />
         <div className="cube_item cube_y" />
@@ -33,27 +34,20 @@ const StyledWrapper = styled.div`
   }
 
   .cube_x {
-    background-color: ${({ $dark }) => ($dark ? 'white' : 'blueviolet')};
+    background-color: ${({ dark }) => (dark ? 'white' : 'blueviolet')};
     animation: animateLoaders 1s infinite;
   }
 
   .cube_y {
-    background-color: ${({ $dark }) => ($dark ? 'darkorange' : '#EAEDEF')};
+    background-color: ${({ dark }) => (dark ? 'darkorange' : '#EAEDEF')};
     animation: animateLoaders 1s 0.5s infinite;
   }
 
   @keyframes animateLoaders {
-    0% {
-      transform: scale(0.8);
-    }
-
-    50% {
-      transform: scale(1.2);
-    }
-
-    100% {
-      transform: scale(0.8);
-    }
-  }`;
+    0% { transform: scale(0.8); }
+    50% { transform: scale(1.2); }
+    100% { transform: scale(0.8); }
+  }
+`;
 
 export default Loader;
