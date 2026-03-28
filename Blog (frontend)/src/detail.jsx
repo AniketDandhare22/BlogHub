6ee36cd998a6c1279c235f513a9f454beba24458
AniketDandhare22 <aniketdandhare22@gmail.com>
2026-03-28 17:30:57 +0530

    update 3.2.3 'feature Added Editting'

--- Blog (frontend)/src/detail.jsx
+++ Blog (frontend)/src/detail.jsx
@@ -159,17 +159,7 @@ function Detail() {
                 </div>
               )}
 
-              {/* Content */}
-              <div className="p-6 space-y-6">
-                {post.type === "blog"  && (
-                <span className="inline-block px-3 py-1 text-xs rounded-full bg-blue-100 text-blue-700 hover:bg-blue-600 hover:text-white transition">
-                  {post.category.toLowerCase()}
-                </span>
-              )}
-
-                <h1 className="text-3xl font-bold light:text-txPrimary text-white leading-tight">
-                  {post.title}
-                </h1>
+              
 
                 <div className="flex w-full items-center justify-between gap-4 text-sm light:text-txPrimary text-white">
                   {/* Left: Author Info */}
@@ -228,10 +218,19 @@ function Detail() {
                     🕛 {getDaysSincePost(post.createdAt)} days
                   </div>
 
-    </div>
+            </div>
 
 
+              {/* Content */}
+              <div className="p-6 space-y-6">
+                <span className="inline-block px-3 py-1 text-xs rounded-full bg-blue-100 text-logo hover:bg-logo light:text-logo2 light:hover:bg-logo2 hover:text-white transition">
+                  {post.category.toLowerCase()}
+                </span>
+              
 
+                <h1 className="text-3xl font-bold light:text-txPrimary text-white leading-tight">
+                  {post.title}
+                </h1>
                 <div>
                   {formatContent(post.detail)}
                 </div>
